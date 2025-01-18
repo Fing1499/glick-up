7e14ca72e54f56afdd185f9b8dd0b76f99ca07c6
fing1499 <findlayg@gmail.com>
2025-01-18 15:34:46 +0000

    8697jfb5w | Testing api

--- src/api/get.js
+++ src/api/get.js
@@ -1,8 +1,6 @@
 import { apiClient } from "./client.js"
 
 const get = async (endpoint) => {
-  console.log(process.env.BASE_URL)
-  console.log(process.env.CLICKUP_API_TOKEN)
   try {
     const response = await apiClient.get(endpoint)
     return response.data;
